908dc0715ebc8845a1274b7e3f67f47301dbea39
구혜연 <guhaa03@daum.net>
2025-04-28 22:34:19 +0900

    ✨ 초기 프로젝트 세팅 완료 (Vite + React + Tailwind)

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Hero from "./components/Hero";
+import Projects from "./components/Projects";
+
+function App() {
+  return (
+    <div >
+    포트폴리오 만드는 중 ..
+ 
+    </div>
+  );
+}
+
+export default App;
