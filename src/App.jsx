67f251533075083e32de41d936f9e9c461a396cb
구혜연 <guhaa03@daum.net>
2025-04-29 10:36:16 +0900

    ✨ Contact 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
@@ -8,6 +9,14 @@ function App() {
       <Navbar />
       <div id="about" className="pt-20">
         <Hero />
+      </div>
+
+      <div id="projects" className="pt-20">
+        <Projects />
+      </div>
+
+      <div id="contact" className="pt-20">
+        <Contact />
       </div>
     </div>
   );
---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+function Contact() {
+  return (
+    <div>
+      <p>Phone : 010-8846-1029</p>
+      <p>Email : ik0n44@naver.com</p>
+      <p>Github : </p>
+      <p>Velog : </p>
+    </div>
+  );
+}
+
+export default Contact;
