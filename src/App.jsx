373f28f2cc14e48ecdac2159cea51a726abbe1e1
구혜연 <guhaa03@daum.net>
2025-04-30 10:39:40 +0900

    🧩 Contact 컴포넌트 삭제, Navbar 컴포넌트 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
-import Contact from "./components/Contact";
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
 import Projects from "./components/Projects";
 
 function App() {
   return (
-    <div >
-      <div id="about">
+    <div>
+      <Navbar />
+      <div id="about" className="mt-20">
         <Hero />
       </div>
 
-      <div id="projects">
+      <div id="projects" className="mt-20">
         <Projects />
       </div>
     </div>
